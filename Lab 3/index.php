400b1127cfec700d146f5033dbe1b539a71fcbd0
Jason <99057964@mydavinci.nl>
2020-04-01 11:44:24 +0200

    Vergetenn
    Was vergeten te pushen

--- Lab 3/index.php
+++ Lab 3/index.php
@@ -1,11 +1,13 @@
 <?php
-    $host = "localhost";//Hostnaam.
-    $name = "root";//Mysql naam.
-    $password = "";//Wachtwoord.
-    $db = "databank_php";//database naam. 
+    $db = 'localhost';
+    $name = 'root';
+    $password = '';
+    $dbname = 'test.php ';
 
-    mysql_connect($host, $name, $password) or die ("Er is een error in gekomen.");//Er word verbinding gemaakt.
-    mysql_select_db($db) or die ("De database naam bestaat helaas niet ");//Er word verbinding gemaakt met de database. 
+    $connection = new mysqli($dbname, $name, $password);
 
-    
+    if($connection->connect_error){
+        die("Fout:" . $connection->connect_error);
+    }
+    echo "Gelukt";
 ?>
